d75e79a9af01ef159a0f937aa60124fd12942436
t kanai <takatoshi.kanai@gmail.com>
2019-12-29 23:50:47 +0900

    first commit

---
+++ bin/main/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO inquiry(name, email, contents, created)
+VALUES('山田太郎', 'sample@example.com', 'Hello', '2019-11-12 08:34:19');
+INSERT INTO inquiry(name, email, contents, created)
+VALUES('鈴木花子', 'sample2@example.com', 'GoodMorning', '2019-12-18 12:10:52');
+INSERT INTO inquiry(name, email, contents, created)
+VALUES('佐藤源十郎', 'sample3@example.com', 'GoodEvening', '2019-12-18 15:10:52');
